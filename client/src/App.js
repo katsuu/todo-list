3b14abf9db92f9735ebc60ed942eaf29d95dc9d4
Katrina Au <katrinaau95@gmail.com>
2020-04-02 18:49:11 +0100

    List of items displayed

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,44 @@
-import React, { useState, useEffect } from "react";
-import "./App.css";
+import React, { useState, useEffect, Component } from "react";
+import Todos from "./Todos";
 
-function App() {
-  const [message, setMessage] = useState("");
-  useEffect(() => {
-    // const fetchMessage = async () => {
-    //   const response = await fetch("http://localhost:3001");
-    //   const data = await response.text();
-    //   setMessage(data);
-    // };
-    const sendMessage = async () => {
-      const response = await fetch("http://localhost:3001", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ message: "Hellooo" })
-      });
-      const data = await response.text();
-      setMessage(data);
-    };
-    // fetchMessage();
-    sendMessage();
-  }, [message, setMessage]);
-  return <h1>{message}</h1>;
+class App extends Component {
+  state = {
+    todos: [
+      { id: 1, content: "buy some milk" },
+      { id: 2, content: "play mario kart" }
+    ]
+  };
+  render() {
+    return (
+      <div className="App">
+        <h1 className="center blue-text">Todo's</h1>
+        <Todos todos={this.state.todos} />
+      </div>
+    );
+  }
 }
+
+// function App() {
+//   const [message, setMessage] = useState("");
+//   useEffect(() => {
+//     // const fetchMessage = async () => {
+//     //   const response = await fetch("http://localhost:3001");
+//     //   const data = await response.text();
+//     //   setMessage(data);
+//     // };
+//     const sendMessage = async () => {
+//       const response = await fetch("http://localhost:3001", {
+//         method: "POST",
+//         headers: { "Content-Type": "application/json" },
+//         body: JSON.stringify({ message: "Hellooo" })
+//       });
+//       const data = await response.text();
+//       setMessage(data);
+//     };
+//     // fetchMessage();
+//     sendMessage();
+//   }, [message, setMessage]);
+//   return <h1>{message}</h1>;
+// }
 
 export default App;
